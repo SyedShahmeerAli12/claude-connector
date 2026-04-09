4b0e33d6745d362dd499d6dae325859eb32d7742
unknown <syedshahmeerali196@gmail.com>
2026-04-09 16:52:29 +0500

    Add shell test script

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -X POST https://aiserver.digitrends.sa:8500/mcp \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json, text/event-stream" \
+  -d '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test","version":"1.0"}}}'
